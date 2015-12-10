76322d3732ef28f76c67432a58b6fd4aa75ae701
Matt Crane <mcrane@snapbug.geek.nz>
2015-12-10 22:33:37 +1300

    Better message for client

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ SHA=$(git log --pretty=format:'%h' -n 1)
 
 # env=windows
 # arch=amd64
-# echo client-${env}-${arch}
+# echo Building client for ${env} ${arch}
 # GOOS=${env} GOARCH=${arch} godep go build -o out/${env}-${arch} -ldflags "-s -X main.version=${SHA}" client.go
 
 # env=windows
 # arch=386
-# echo client-${env}-${arch}
+# echo Building client for ${env} ${arch}
 # GOOS=${env} GOARCH=${arch} godep go build -o out/${env}-${arch} -ldflags "-s -X main.version=${SHA}" client.go
 
 env=darwin
 arch=amd64
-echo client-${env}-${arch}
+echo Building client for ${env} ${arch}
 GOOS=${env} GOARCH=${arch} godep go build -o out/${env}-${arch} -ldflags "-s -X main.version=${SHA}" client.go
