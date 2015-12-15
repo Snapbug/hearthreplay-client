9e513f5f840a6f166445c6c9ea11c3e6ffecc10d
Matt Crane <mcrane@snapbug.geek.nz>
2015-12-15 20:50:29 +1300

    Simpler aws syncing

--- build.sh
+++ build.sh
@@ -13,22 +13,24 @@ echo "{\"version\": \"${SHA}\"}" >| ../server/tmpl/client.json
 # commit the change -- creating a new sha -- oh well!
 git commit -m "Updating client version" ../server/tmpl/client.json
 
+mkdir out/${SHA}
+
 env=windows
 arch=amd64
 echo Building client for ${env} ${arch}
-GOOS=${env} GOARCH=${arch} godep go build -o out/hsrclient-${env}-${arch}-${SHA} -ldflags "-s -X main.version=${SHA}" client.go bindata.go
+GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/hsrclient-${env}-${arch}-${SHA} -ldflags "-s -X main.version=${SHA}" client.go bindata.go
 
 env=windows
 arch=386
 echo Building client for ${env} ${arch}
-GOOS=${env} GOARCH=${arch} godep go build -o out/hsrclient-${env}-${arch}-${SHA} -ldflags "-s -X main.version=${SHA}" client.go bindata.go
+GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/hsrclient-${env}-${arch}-${SHA} -ldflags "-s -X main.version=${SHA}" client.go bindata.go
 
 env=darwin
 arch=amd64
 echo Building client for ${env} ${arch}
-GOOS=${env} GOARCH=${arch} godep go build -o out/hsrclient-${env}-${arch}-${SHA} -ldflags "-s -X main.Version=${SHA}" client.go bindata.go
+GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/hsrclient-${env}-${arch}-${SHA} -ldflags "-s -X main.Version=${SHA}" client.go bindata.go
 
 echo Updating version to s3
-aws s3 sync out/*${SHA} s3://update.hearthreplay.com --acl public-read
+aws s3 sync out/${SHA} s3://update.hearthreplay.com --acl public-read
 
 echo Push the changes, pull on server
