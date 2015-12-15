61f8bbe81ad69d457397b58d85e635905aa0f316
Matt Crane <mcrane@snapbug.geek.nz>
2015-12-15 20:37:18 +1300

    Added bootstrap-builder and old index file

---
+++ bootstrap-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+env=windows
+arch=amd64
+echo Building client for ${env} ${arch}
+GOOS=${env} GOARCH=${arch} godep go build -o out/hearthreplay-updater-${env}-${arch}.exe -ldflags "-s" bootstrap.go
+
+env=windows
+arch=386
+echo Building bootstrap for ${env} ${arch}
+GOOS=${env} GOARCH=${arch} godep go build -o out/hearthreplay-updater-${env}-${arch}.exe -ldflags "-s" bootstrap.go
+
+env=darwin
+arch=amd64
+echo Building client for ${env} ${arch}
+GOOS=${env} GOARCH=${arch} godep go build -o hearthreplay-updater-${env}-${arch} -ldflags "-s" bootstrap.go
+
+aws s3 sync out/ s3://update.hearthreplay.com --acl public-read
