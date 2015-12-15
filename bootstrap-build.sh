62d977a982c86c755a4285439bc12a6fc575a30e
Matt Crane <mcrane@snapbug.geek.nz>
2015-12-15 21:42:54 +1300

    Added folder for bootstrap showing

--- bootstrap-build.sh
+++ bootstrap-build.sh
@@ -3,12 +3,12 @@ env=windows
 for arch in amd64 386
 do
 	echo Building client for ${env} ${arch}
-	GOOS=${env} GOARCH=${arch} godep go build -o out/hearthreplay-updater-${env}-${arch}.exe -ldflags "-s" bootstrap.go
+	GOOS=${env} GOARCH=${arch} godep go build -o out/boot/hearthreplay-updater-${env}-${arch}.exe -ldflags "-s" bootstrap.go
 done
 
 env=darwin
 arch=amd64
 echo Building client for ${env} ${arch}
-GOOS=${env} GOARCH=${arch} godep go build -o out/hearthreplay-updater-${env}-${arch} -ldflags "-s" bootstrap.go
+GOOS=${env} GOARCH=${arch} godep go build -o out/boot/hearthreplay-updater-${env}-${arch} -ldflags "-s" bootstrap.go
 
-aws s3 sync out/ s3://update.hearthreplay.com --acl public-read --exclude ".DS_Store"
+aws s3 sync out/boot/ s3://update.hearthreplay.com --acl public-read --exclude ".DS_Store"
