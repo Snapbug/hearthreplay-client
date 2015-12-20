053fe2c6c3ec8d848629e3bd3a0944799833cb8d
Matt Crane <mcrane@snapbug.geek.nz>
2015-12-20 17:37:22 +1300

    Fixed deploy issues

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+ROOT="hearthreplay-client"
+
 function doclient() {
 	j="../server/tmpl/client.$1.$2.json"
-	p="out/$3/hsrclient-$1-$2-$3"
+	p="out/$3/${ROOT}-$1-$2-$3"
 	checksum=$(openssl dgst -sha256 ${p} | sed 's/^.* //')
 
 	signature=$(openssl dgst -sha256 -sign private.pem -keyform PEM ${p} | base64)
@@ -27,14 +29,14 @@ env=windows
 for arch in amd64 386
 do
 	echo Building client for ${env} ${arch}
-	GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/hearthreplay-client-${env}-${arch}-${SHA} -ldflags "-s -H windowsgui -X main.version=${SHA}" client.go bindata.go
+	GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/${ROOT}-${env}-${arch}-${SHA} -ldflags "-s -H windowsgui -X main.version=${SHA}" client.go bindata.go
 	doclient ${env} ${arch} ${SHA}
 done
 
 env=darwin
 arch=amd64
 echo Building client for ${env} ${arch}
-GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/hearthreplay-client-${env}-${arch}-${SHA} -ldflags "-s -X main.Version=${SHA}" client.go bindata.go
+GOOS=${env} GOARCH=${arch} godep go build -o out/${SHA}/${ROOT}-${env}-${arch}-${SHA} -ldflags "-s -X main.Version=${SHA}" client.go bindata.go
 doclient ${env} ${arch} ${SHA}
 
 echo Updating version to s3
